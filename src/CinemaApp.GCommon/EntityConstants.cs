8c80c5c2e12f48c3afc5330e102e9f1942c11b2b
Deivid Silviev Yordanov <deividyordanov@abv.bg>
2026-03-08 12:19:20 +0200

    Minimum length constants added

--- src/CinemaApp.GCommon/EntityConstants.cs
+++ src/CinemaApp.GCommon/EntityConstants.cs
@@ -10,16 +10,24 @@ namespace CinemaApp.GCommon
     {
         //Movie entity constants
         public const int MovieTitleMaxLength = 100;
-        
+        public const int MovieTitleMinLength = 1;
+
+        public const int MovieReleaseYearMinValue = 1888;
+        public const int MovieReleaseYearMaxValue = 2026;
+
         public const int MovieGenreMaxLength = 50;
-        
+        public const int MovieGenreMinLength = 1;
+
         public const int MovieDirectorMaxLength = 100;
-        
+        public const int MovieDirectorMinLength = 1;
+
         public const int MovieDescriptionMaxLength = 1000;
+        public const int MovieDescriptionMinLength = 1;
 
         public const int DurationMinValue = 1;
         public const int DurationMaxValue = 300;
 
         public const int UrlMaxLength = 2048;
+        public const int UrlMinLength = 1;
     }
 }
