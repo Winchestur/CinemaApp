6c871974b2c9912fe4a5b992bc2e915e897694c9
Deivid Silviev Yordanov <deividyordanov@abv.bg>
2026-03-08 12:22:19 +0200

    Added AllMoviesIndexViewModel to use in controllers and views

---
+++ src/CinemaApp.Web.ViewModels/Movie/AllMoviesIndexViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static CinemaApp.GCommon.EntityConstants;
+
+namespace CinemaApp.Web.ViewModels.Movie
+{
+    public class AllMoviesIndexViewModel
+    {
+        [Key]
+        public string Id { get; set; } = null!;
+
+        [Required]
+        [StringLength(MovieTitleMaxLength, MinimumLength = MovieTitleMinLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(MovieGenreMaxLength, MinimumLength = MovieGenreMinLength)]
+        public string Genre { get; set; } = null!;
+
+        [Required]
+        public string ReleaseDate { get; set; } = null!;
+
+        [Required]
+        [StringLength(MovieDirectorMaxLength, MinimumLength = MovieDirectorMinLength)]
+        public string Director { get; set; } = null!;
+
+        [Required]
+        [StringLength(DurationMaxValue, MinimumLength = DurationMinValue)]
+        public string Duration { get; set; } = null!;
+
+        [Required]
+        [StringLength(UrlMaxLength, MinimumLength = UrlMinLength)]
+        public string? ImageUrl { get; set; }
+    }
+}
